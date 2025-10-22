fb206f0ab2a9df89180cd7d7dc55ce22e0d1d89a
Renan Fortunato <renanfortunatosilveira@gmail.com>
2025-10-22 10:55:31 -0300

    acerto do create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,3 +1,7 @@
+-- Sequences
+CREATE SEQUENCE paciente_id_seq INCREMENT BY 1 START WITH 1;
+CREATE SEQUENCE consulta_id_seq INCREMENT BY 1 START WITH 1;
+
 -- Tabelas
 CREATE TABLE Medico (
     crm VARCHAR(15) PRIMARY KEY,
@@ -8,7 +12,7 @@ CREATE TABLE Medico (
 );
 
 CREATE TABLE Paciente (
-    id_paciente INTEGER PRIMARY KEY DEFAULT nextval('paciente_id_seq'),
+    id_paciente INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     data_nascimento DATE NOT NULL,
     cpf VARCHAR(14) UNIQUE NOT NULL,
@@ -16,26 +20,22 @@ CREATE TABLE Paciente (
 );
 
 CREATE TABLE Consulta (
-    id_consulta INTEGER PRIMARY KEY DEFAULT nextval('consulta_id_seq'),
+    id_consulta INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     crm_medico VARCHAR(15) NOT NULL,
     id_paciente INTEGER NOT NULL,   
     data_hora TIMESTAMP NOT NULL,
-    observacoes TEXT,
+    observacoes VARCHAR2(3000),
     
     CONSTRAINT fk_medico
         FOREIGN KEY (crm_medico)
-        REFERENCES Medico (crm)
-        ON DELETE RESTRICT, 
+        REFERENCES Medico (crm),
         
     CONSTRAINT fk_paciente
         FOREIGN KEY (id_paciente)
         REFERENCES Paciente (id_paciente)
-        ON DELETE RESTRICT 
 );
 
--- Sequences
-CREATE SEQUENCE paciente_id_seq INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE consulta_id_seq INCREMENT BY 1 START WITH 1;
+
 
 
 -- 4. Criação de índices para otimizar consultas por especialidade e paciente
